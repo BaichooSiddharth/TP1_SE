fa05985ff276ee264b897f6a992c249a15c1f852
ntnco <antoinefcolson@gmail.com>
2021-02-11 01:37:31 -0500

    finish x4

--- src/main.c
+++ src/main.c
@@ -45,15 +45,18 @@ struct command {
     struct command *next;
     int count;
     bool also;
+    int rTimes;
 };
 
-struct command *new_node(char** n_call, enum op n_op, int n_count, bool n_also) {
+struct command *new_node(char** n_call, enum op n_op,
+        int n_count, bool n_also, int rTimes) {
     struct command *node = malloc(sizeof(struct command));
     node->call = n_call;
     node->operator = n_op;
     node->count = n_count;
     node->also = n_also;
     node->next = NULL; //bonne pratique!
+    node->rTimes = rTimes;
     return node;
 }
 
@@ -101,7 +104,8 @@ error_code readline(char **out) {
     size_t size = 8;                       // size of the char array
     char *line = malloc(sizeof(char) * size);       // initialize a ten-char line
     if (line == NULL)
-        return ERROR; // TODO: don't return error on empty input
+        return ERROR;
+    line[0] = '\0';
 
     char ch;
     int at = 0;
@@ -112,6 +116,10 @@ error_code readline(char **out) {
         if (at >= size) {
             size *= 2;
             line = realloc(line, sizeof(char) * size);
+            if (!line) {
+                printf("failed%c", '\n');
+                exit(0);
+            }
         }
     }
 
@@ -152,6 +160,52 @@ char **parseWords(char *line, int *numWords) {
     return words;
 }
 
+bool isDigit(char c) {
+    return c == '0' ||
+    c == '1' ||
+    c == '2' ||
+    c == '3' ||
+    c == '4' ||
+    c == '5' ||
+    c == '6' ||
+    c == '7' ||
+    c == '8' ||
+    c == '9';
+}
+
+int check_rN(char **call, int numWords) {
+    char *first = *call;
+    int rN = 0;
+    int i = 1;
+
+    if (first[0] != 'r')
+        return 1;
+    else {
+        char cur;
+        while ((cur = first[i++]) != '(') {
+            if (isDigit(cur)) {
+                rN *= 10;
+                rN += (int) cur - 48;
+            } else
+                return 1;
+        }
+    }
+    char *lastWord = call[numWords - 1];
+    int iLast = strlen(lastWord) - 1;
+    char lastChar = lastWord[iLast];
+    if (lastChar != ')')
+        return 1;
+
+    lastWord[iLast] = '\0';
+
+    for (int j = 0; j <= iLast; j++){
+        call[0][j] = call[0][j + i];
+    }
+
+    return rN;
+
+}
+
 struct command *parseLine(char *line) {
     int numWords;
     char **words = parseWords(line, &numWords);
@@ -180,7 +234,9 @@ struct command *parseLine(char *line) {
         }
         if (symbol != BIDON || i == numWords - 1) { //word is a symbol or end of line
             currentCall[j] = NULL;
-            nextNode = new_node(currentCall, symbol, j, symbol == ALSO);
+
+            int rn = check_rN(currentCall, j);
+            nextNode = new_node(currentCall, symbol, j, symbol == ALSO, rn);
 
             if (!currentNode) { // current node is first node
                 currentNode = nextNode;
@@ -209,15 +265,17 @@ int runNode(struct command *head) {
     pid_t pid;
     int status;
 
-    pid = fork();
-    if (pid == 0) {
-        char *file = *head->call;
-        error_code e = execvp(file, head->call);
-        freeStringArray(head->call);
-        printf("encountered error %i\n", e);
-        exit(e);
-    } else
-        waitpid(pid, &status, 0);
+    for (int i = 0; i < head->rTimes; i++) {
+        pid = fork();
+        if (pid == 0) {
+            char *file = *head->call;
+            error_code e = execvp(file, head->call);
+            freeStringArray(head->call);
+            printf("encountered error %i\n", e);
+            exit(e);
+        } else
+            waitpid(pid, &status, 0);
+    }
 
     if (status == 0)
         while (head && head->operator != AND)
@@ -232,6 +290,8 @@ int runNode(struct command *head) {
     }
     else
         runNode(head->next);
+
+    return 0; // will ne run
 }
 
 bool checkIfLastAlso(struct command *node){
